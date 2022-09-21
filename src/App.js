098818e56858ada49a6cd4c78f6ead981956bcec
Ramazan USLU <ruslu1720@gmail.com>
2022-09-22 00:03:06 +0300

    Accordion Menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [seleced, setSelected] = useState(null);
+  const toggle = (i) => {
+    if (seleced === i) {
+      return setSelected(null);
+    }
+    setSelected(i);
+  };
+  const toggleLeave = (i) => {
+    if (seleced === i) {
+      return setSelected(null);
+    }
+    setSelected(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="accordion">
+        {data.map((item, i) => (
+          <div key={i} className="item">
+            <div
+              className="title"
+              onMouseEnter={() => toggle(i)}
+              onClick={() => console.log("selam")}
+            >
+              <h2>{item.question}</h2>
+              <span>{seleced === i ? "-" : "+"}</span>
+            </div>
+            <div
+              onMouseLeave={() => toggleLeave(i)}
+              className={seleced === i ? "content show" : "content"}
+            >
+              {item.answer}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
+const data = [
+  {
+    question: "Question 1",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ex, iste deleniti nihil vero eligendi culpa! At quam pariatur ad officiis impedit laudantium, necessitatibus corporis ea, asperiores provident ullam aliquid quas beatae consectetur consequuntur assumenda ipsum placeat, sed odit quis cum. Ipsum laboriosam reiciendis itaque alias ullam consequatur doloribus nam?",
+  },
+  {
+    question: "Question 2",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ex, iste deleniti nihil vero eligendi culpa! At quam pariatur ad officiis impedit laudantium, necessitatibus corporis ea, asperiores provident ullam aliquid quas beatae consectetur consequuntur assumenda ipsum placeat, sed odit quis cum. Ipsum laboriosam reiciendis itaque alias ullam consequatur doloribus nam?",
+  },
+  {
+    question: "Question 3",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ex, iste deleniti nihil vero eligendi culpa! At quam pariatur ad officiis impedit laudantium, necessitatibus corporis ea, asperiores provident ullam aliquid quas beatae consectetur consequuntur assumenda ipsum placeat, sed odit quis cum. Ipsum laboriosam reiciendis itaque alias ullam consequatur doloribus nam?",
+  },
+  {
+    question: "Question 4",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ex, iste deleniti nihil vero eligendi culpa! At quam pariatur ad officiis impedit laudantium, necessitatibus corporis ea, asperiores provident ullam aliquid quas beatae consectetur consequuntur assumenda ipsum placeat, sed odit quis cum. Ipsum laboriosam reiciendis itaque alias ullam consequatur doloribus nam?",
+  },
+  {
+    question: "Question 5",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi ex, iste deleniti nihil vero eligendi culpa! At quam pariatur ad officiis impedit laudantium, necessitatibus corporis ea, asperiores provident ullam aliquid quas beatae consectetur consequuntur assumenda ipsum placeat, sed odit quis cum. Ipsum laboriosam reiciendis itaque alias ullam consequatur doloribus nam?",
+  },
+];
 
 export default App;
